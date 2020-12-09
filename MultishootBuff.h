ee3916b7e52f95cf3547d7515eed6d5b4f9e2ff7
charaz17 <hubertch1999@gmail.com>
2020-12-09 18:37:17 +0100

    score

---
+++ MultishootBuff.h
@@ -0,0 +1,23 @@
+#ifndef MULTISHOOTBUFF_H
+#define MULTISHOOTBUFF_H
+//#include "Buff.h"
+#include "Ship.h"
+#include <QGraphicsPixmapItem>
+#include <QGraphicsScene>
+#include <QPixmap>
+#include <QTimer>
+
+class MultishootBuff : public QObject, public QGraphicsPixmapItem{
+       Q_OBJECT
+       private:
+           QPixmap image;
+       public:
+           MultishootBuff(int x, int y);
+           ~MultishootBuff();
+
+           void move();
+           void checkForCollision();
+};
+
+#endif // MULTISHOOTBUFF_H
+
